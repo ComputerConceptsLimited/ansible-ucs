97fbb011c25a31b0043609c24597c796f40555fe
depereo <alex.white-robinson@concepts.co.nz>
2017-07-10 17:21:22 +1200

    improving documentation

--- library/ucs_vnic_template_vlan.py
+++ library/ucs_vnic_template_vlan.py
@@ -16,14 +16,46 @@ author: Alex White-Robinson
 extends_documentation_fragment: ucs
 
 options: 
-    vlan_name           (Required)
-    vnic_template_name  (Required)
-    org (Required - list of org heirarchy vnic template lives in. 
-            For example, for root/infra provide ['root', 'infra'], for root/infra/client provide ['root', 'infra', 'client'].
-    policy owner        (defaults to local - options are local, policy or pending-policy)
-    hostname            (Required)
-    username            (Required)
-    password            (Required)
+    vlan_name:
+        description:
+            - Single VLAN ID
+        required: true
+        default: null
+    vnic_template_name:
+        description:
+            - Name of vnic template to update.
+              Example "vm_data_a" or "vmotion_a"
+              Must exist in the Organization specified in 'org'
+        required: true
+        default: null
+    org:
+        description: 
+            - List of org heirarchy vnic template lives in.
+              For example, for root/ccl provide ['root', 'infra'].
+              For root/infra/client provide ['root', 'infra', 'client'].
+        required: true
+        default: null
+    policy owner:
+        description: 
+            - Owner of the vnic template
+        choices: ['local', 'policy', 'pending-policy']
+        required: false
+        default: local
+'''
+
+EXAMPLES = '''
+
+- name: Adding vlan client_10 to vnic template guest_a and guest_b
+    ucs_vnic_template_vlan:
+      ip={{ucsm_ip}}
+      username={{ucsm_login}}
+      password={{ucsm_pw}}
+      vlan_name= "client_10"
+      org=['root', 'infra', 'guest']
+      vnic_template_name: {{item}}
+    with_items:
+        - 'guest_a'
+        - 'guest_b'
 
 '''
 
@@ -50,18 +82,13 @@ def modify_vnic_template_vlan(module):
     for org_name in org:
         org_dn = ''
         org_dn += str('org-' + org_name + '/')
-    org_vnic_templ_dn += str("lan-conn-templ-" + vnic_template_name)
+    org_dn += str("lan-conn-templ-" + vnic_template_name
 
-    mo_a = VnicEtherIf(parent_mo_or_dn=org_vnic_templ_dn, addr="derived", child_action="deleteNonPresent", 
+    mo = VnicEtherIf(parent_mo_or_dn=org_dn, addr="derived", child_action="deleteNonPresent", 
                         default_net="no", flt_aggr="0", name=str(vlan_name), oper_state="indeterminate", 
                         owner="logical", pub_nw_id="0", sharing="primary", switch_id="A", type="ether", vnet="1")
 
-    mo_b = VnicEtherIf(parent_mo_or_dn=org_dn, addr="derived", child_action="deleteNonPresent", 
-                        default_net="no", flt_aggr="0", name=str(vlan_name), oper_state="indeterminate", 
-                        owner="logical", pub_nw_id="0", sharing="primary", switch_id="A", type="ether", vnet="1")
-
-    ucsm.handle.add_mo(mo_a)
-    ucsm.handle.add_mo(mo_b)
+    ucsm.handle.add_mo(mo)
     
     try:
         ucsm.handle.commit()
